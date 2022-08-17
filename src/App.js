653a2a8ea2e21d32c027201a8a06a05b1a70a1df
orock glen <glen19.og@gmail.com>
2022-08-17 16:28:53 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -7,10 +7,28 @@ import './App.css';
 function App() {
   const [input,setInput] = useState(0);
   const [output,setOutput] = useState(0);
-  
+  const [waiting,setWaiting] = useState(false);
   const calculateSum = debounce((value) => {
-    setOutput(primeNumberSumCalculator(value))
+    if(value >= 50){
+      let promise = new Promise((res) => {
+        res(primeNumberSumCalculator(value))
+        console.log('here')
+      })
+      promise.then((res) => {
+        setOutput(res)
+        setWaiting(false)
+      })
+    }else{
+      setOutput(primeNumberSumCalculator(value))
+    }
+    
   },3000);
+
+  const wait = debounce((value) => {
+    if(value >= 50){
+      setWaiting(true)
+    }
+  },2000);
 
   useEffect(() => {
     calculateSum.cancel()
@@ -21,15 +39,19 @@ function App() {
         value={input}
         onChange={(e) => {
           setInput(e.target.value)
+          wait(e.target.value)
           calculateSum(e.target.value)
         }}
         type='number'
+        disabled={waiting}
       />
       <div>
         {output}
       </div>
+      <div>
+        {waiting?"Calculating result":""}
+      </div>
     </div>
   );
 }
-
 export default App;
